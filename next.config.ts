63934696ff2c87cde1cd775299a6c0e49c3c6901
GetParadisePay <getparadisepay@gmail.com>
2025-12-02 09:36:54 +0000

    fixed @mui /grid

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
+  },
+  experimental: {
+    optimizePackageImports: ["@mui/material", "@mui/icons-material"]
   }
 };
 
